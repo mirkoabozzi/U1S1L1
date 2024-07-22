f2fea3c0678e0fafbccd3410d649fb3ecbca4b2c
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-22 16:08:10 +0200

    es.1 corretto

--- src/Es1.java
+++ src/Es1.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
+
 public class Es1 {
     public static void main(String[] args) {
 
-        String[] arr={"M","", "r","k","o"};
+        String[] arr={"M","r","k","o"};
         String i= "i";
 
         System.out.println(moltiplicazione(5,2));
         System.out.println(concatena("Sono ", "Mirko"));
-        System.out.println(inserisciInArray(arr, i));
+        System.out.println(Arrays.toString(inserisciInArray(arr, i)));
 
 
     }
@@ -19,8 +21,13 @@ public class Es1 {
     }
 
     public static String[] inserisciInArray(String[] array, String str3 ) {
-        array = new String[5];
-        array[2] = str3;
-        return array;
+        String[] newArray = new String[5];
+        newArray[0]= array[0];
+        newArray[1] = str3;
+        newArray[2]= array[1];
+        newArray[3]= array[2];
+        newArray[4]= array[3];
+
+        return newArray;
     }
 }
